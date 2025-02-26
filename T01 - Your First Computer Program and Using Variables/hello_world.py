6fcc2314e9339b7db276d5b1c52951e407c08bcc
Raul <ra.gavris@gmail.com>
2025-02-26 21:26:02 +0100

    Create hello_world.py

---
+++ T01 - Your First Computer Program and Using Variables/hello_world.py
@@ -0,0 +1,10 @@
+# Take in a user’s name using input() and then print out the name
+name = input("Your name: ")
+print(name)
+
+# Use the same input and output approach to take in a user’s age and print it out
+age = input("Your age: ")
+print(age)
+
+# Print the string “Hello World!” on a new line
+print("Hello World!")
