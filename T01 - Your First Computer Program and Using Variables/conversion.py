55091388908dbc6b3226276356140fa27753029e
Raul <ra.gavris@gmail.com>
2025-02-26 21:26:32 +0100

    Add files via upload

---
+++ T01 - Your First Computer Program and Using Variables/conversion.py
@@ -0,0 +1,24 @@
+# Declare the following variables:
+# num1 = 99.23
+# num2 = 23
+# num3 = 150
+# string1 = “100”
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# Convert them as follows:
+# num1 into an integer
+num1 = int(num1)
+# num2 into a float
+num2 = float(num2)
+# num3 into a String
+num3 = str(num3)
+# string1 into an integer
+string1 = int(string1)
+# Print out all the variables on separate lines
+print(num1)
+print(num2)
+print(num3)
+print(string1)
---
+++ T01 - Your First Computer Program and Using Variables/details.py
@@ -0,0 +1,8 @@
+# Get the user input for name, age, house number and street name
+user_name = input("Write your name: ") 
+user_age = input("Write your age: ")
+user_house_number = input("Write your house number: ")
+user_street_name = input("Write your street name: ")
+
+# Print out a single sentence containing all the details of the user
+print(f"Hi, my name is {user_name} and I'm {user_age} years old. My address is {user_house_number} {user_street_name}.")
